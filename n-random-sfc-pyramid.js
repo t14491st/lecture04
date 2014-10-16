c23771d8675a18f556d8fea457a531dead95dfa4
t14491st <sonosuke.takao@gmail.com>
2014-10-16 13:30:38 +0900

    課題作成

---
+++ n-random-sfc-pyramid.js
@@ -0,0 +1,30 @@
+
+var nSFC = function(n){
+  var message = "";
+  var counter = 0;
+ 
+  while(counter < n){
+    message = message + "SFC ";
+    counter = counter + 1;
+  }
+  
+  return message;
+};
+
+
+var nRandomSFCPyramid = function(n){
+  var n = Math.floor(Math.random() * 100);
+ 
+  var output = "";
+  var row = 1
+  
+  while(row <= n){
+   output = nSFC(row);
+   console.log(output);
+   row = row + 1;
+   };
+  return output;
+  };
+
+
+nRandomSFCPyramid()
